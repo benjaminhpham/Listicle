f69ab44aeee70e24f8198b16c64386489630e829
Benjamin Huy Pham <benjaminhpham@gmail.com>
2024-10-02 23:45:24 -0600

    create queries for creating airport table and seeding data to that table

---
+++ server/config/reset.js
@@ -0,0 +1,67 @@
+import { pool } from "./database";
+import "./dotenv";
+import airportData from "../data/airportData";
+
+const createAirportsTable = async () => {
+  const createAirportsQuery = `
+    DROP TABLE IF EXISTS airports;
+    
+    CREATE TABLE IF NOT EXISTS airports (
+      id SERIAL PRIMARY KEY,
+      rank INTEGER NOT NULL,
+      name VARCHAR(255) NOT NULL,
+      location VARCHAR(255) NOT NULL,
+      country VARCHAR(255) NOT NULL,
+      code VARCHAR(255) NOT NULL,
+      image TEXT NOT NULL,
+      passengers INTEGER NOT NULL,
+      percentageChange DOUBLE NOT NULL,
+    )
+  `;
+
+  try {
+    await pool.query(createAirportsQuery);
+    console.log("Airports table created successfully");
+  } catch (err) {
+    console.error("Error creating Airports table", err);
+  }
+};
+
+const seedAirportsTable = async () => {
+  await createAirportsTable();
+
+  airportData.forEach((airport) => {
+    const {
+      rank,
+      name,
+      location,
+      country,
+      code,
+      image,
+      passengers,
+      percentageChange,
+    } = airport;
+
+    const insertQuery = `INSERT INTO airports (rank, name, location, country, code, image, passengers, percentageChange) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+
+    const values = [
+      rank,
+      name,
+      location,
+      country,
+      code,
+      image,
+      passengers,
+      percentageChange,
+    ];
+
+    pool.query(insertQuery, values, (err, res) => {
+      if (err) {
+        return console.error("Error inserting airport", err);
+      }
+      console.log(`${airport.name} added successfully`);
+    });
+  });
+};
+
+seedAirportsTable();
